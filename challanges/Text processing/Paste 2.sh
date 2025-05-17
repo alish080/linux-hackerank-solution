2b1b93174c43c6820c64b5bc9a0e6aedeea3559a
alish080 <alishthapa45@gmail.com>
2025-05-17 10:45:10 +0545

    Update and rename Paste 2-sh to Paste 2.sh

---
+++ challanges/Text processing/Paste 2.sh
@@ -0,0 +1,3 @@
+#Here we just use -d ";" as it diver semicolan after every word  - - - gives our 3 word dsitance - - defines 2 word 
+#seperation and - - - defines 3 words sepertion from next line .
+paste -d ";" - - - 
