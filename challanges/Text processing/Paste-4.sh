63962e53439d32f1627849198346cf9e1c6190de
alish080 <alishthapa45@gmail.com>
2025-05-19 19:34:28 +0545

    Create Paste-4.sh

---
+++ challanges/Text processing/Paste-4.sh
@@ -0,0 +1,3 @@
+#Here, what we have done is we need the three word in each line from the input row where after the three word there must be tab so we use
+# -d "-tab-" - - - which helps us to put three word in one line and tab after each word in each row 
+ paste -d "  " - - - 
